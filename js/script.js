59b7e0a5c1a9c1c62577266eddc48edfee7733e0
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-22 15:50:32 +0100

    stamp 10 mail in a for cicle

--- js/script.js
+++ js/script.js
@@ -8,9 +8,11 @@ Buon Lavoro*/
 
 //Axios Call
 
-axios
-  .get('https://flynn.boolean.careers/exercises/api/random/mail')
-  .then((response) => {
-    const randomMail = response.data.response;
-    console.log(randomMail);
-  });
+for (let i = 0; i < 10; i++) {
+  axios
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response) => {
+      const randomMail = response.data.response;
+      console.log(randomMail);
+    });
+}
