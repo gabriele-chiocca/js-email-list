e17a6c442be89db52b07ec4305af7fe2506d4999
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-22 16:10:18 +0100

    stamp new 10 mail from button click

--- js/script.js
+++ js/script.js
@@ -6,17 +6,39 @@ Abbellire con CSS o Bootstrap
 Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 Buon Lavoro*/
 
-//Axios Call
+// Const
 const rowForEmail = document.getElementById('emailDivRow');
 
+const buttonNewMail = document.getElementById('buttonstampnewmail');
+
+//Event
+
+buttonNewMail.addEventListener('click', function () {
+  rowForEmail.innerHTML = ``;
+
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((response) => {
+        const randomMail = response.data.response;
+
+        rowForEmail.innerHTML += `<div class="col gy-3">
+          <h2 class="fs-200 text-center">${randomMail}</h2>`;
+
+        console.log(randomMail);
+      });
+  }
+});
+
+//Axios Call in For Cicle
 for (let i = 0; i < 10; i++) {
   axios
     .get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then((response) => {
       const randomMail = response.data.response;
 
-      rowForEmail.innerHTML += `<div class="col">
-          <h2 class="fs-200">${randomMail}</h2>`;
+      rowForEmail.innerHTML += `<div class="col gy-3">
+          <h2 class="fs-200 text-center">${randomMail}</h2>`;
 
       console.log(randomMail);
     });
