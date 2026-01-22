115cf91bc6fd9fabb360f00559c032513f93735d
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-22 15:49:42 +0100

    First call axios generate single mail

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 Buon Lavoro*/
+
+//Axios Call
+
+axios
+  .get('https://flynn.boolean.careers/exercises/api/random/mail')
+  .then((response) => {
+    const randomMail = response.data.response;
+    console.log(randomMail);
+  });
